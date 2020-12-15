0fa83bcf7df363e47db31939b8069a88cad7cdf8
JR Rickerson <jrrickerson@redrivetstudios.com>
2020-12-15 15:00:12 -0500

    Add Day 4 Part 2 solution and tests

--- python/day004.py
+++ python/day004.py
@@ -1,6 +1,17 @@
 '''Advent of Code 2020
    Day 4 Solution - Passport Processing
 '''
+import re
+
+RE_VALID_HAIR_COLOR = '^#[0-9a-f]{6}$'
+RE_VALID_PID = '^[0-9]{9}$'
+
+
+def validate_height(value):
+    return (
+        (value.endswith('cm') and (150 <= int(value[:-2]) <= 193)) or
+        (value.endswith('in') and (59 <= int(value[:-2]) <= 76))
+    )
 
 
 def parse_passports(data):
@@ -31,16 +42,23 @@ def parse_passport(passport_string):
         pair.split(':') for pair in passport_string.split()])
 
 
-def validate_passport(passport, required_keys):
-    return set(required_keys) <= set(passport.keys())
+def validate_passport(passport, required_keys, validation_rules={}):
+    if not set(required_keys) <= set(passport.keys()):
+        return False
+    for key, rule in validation_rules.items():
+        if not rule(passport.get(key)):
+            return False
+        #print(passport.get(key), 'valid for', key)
+    return True
 
 
-def count_valid_passports(data, required_fields=None):
+def count_valid_passports(
+        data, required_fields=None, validation_rules={}):
     required_fields = required_fields or None
     valid_passports = 0
     passports = parse_passports(data)
     for passport in passports:
-        valid = validate_passport(passport, required_fields)
+        valid = validate_passport(passport, required_fields, validation_rules)
         if valid:
             valid_passports += 1
 
@@ -65,7 +83,37 @@ def solve(filename):
     return count
 
 
+def solve2(filename):
+    required_fields = [
+        'byr',
+        'iyr',
+        'eyr',
+        'hgt',
+        'hcl',
+        'ecl',
+        'pid',
+    ]
+    validation_rules = {
+        'byr': lambda x: 1920 <= int(x) <= 2002,
+        'iyr': lambda x: 2010 <= int(x) <= 2020,
+        'eyr': lambda x: 2020 <= int(x) <= 2030,
+        'hgt': validate_height,
+        'hcl': lambda x: re.match(RE_VALID_HAIR_COLOR, x),
+        'ecl': lambda x: x in (
+            'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'),
+        'pid': lambda x: re.match(RE_VALID_PID, x),
+    }
+
+    with open(filename) as f:
+        count = count_valid_passports(
+            f, required_fields=required_fields,
+            validation_rules=validation_rules)
+
+    return count
+
+
 if __name__ == '__main__':
     solution = solve('data/day004.in')
     print('Day 004 (Part 1): ', solution)
-
+    solution = solve2('data/day004.in')
+    print('Day 004 (Part 2): ', solution)
--- python/tests/test_day004.py
+++ python/tests/test_day004.py
@@ -1,4 +1,5 @@
 import io
+import re
 
 from .. import day004
 
@@ -128,6 +129,100 @@ def test_validate_passport_invalid():
     assert valid is False
 
 
+def test_validate_passport_empty_validation():
+    passport = {
+        'ecl': 'gry',
+        'pid': '860033327',
+        'eyr': '2020',
+        'hcl': '#fffffd',
+        'byr': '1937',
+        'iyr': '2017',
+        'cid': '147',
+        'hgt': '183cm'
+    }
+    required_keys = ('ecl', 'pid', 'eyr', 'hcl')
+    valid = day004.validate_passport(
+        passport, required_keys,
+        validation_rules={})
+
+    assert valid is True
+
+
+def test_validate_passport_validation_rules():
+    passport = {
+        'ecl': 'gry',
+        'pid': '860033327',
+        'eyr': '2020',
+        'hcl': '#fffffd',
+        'byr': '1937',
+        'iyr': '2017',
+        'cid': '147',
+        'hgt': '183cm'
+    }
+    required_keys = ('ecl', 'pid', 'eyr', 'hcl')
+    validation_rules = {
+            'byr': lambda x: 1920 <= int(x) <= 2002,
+            'hcl': lambda x: re.match('#[0-9a-f]{6}', x)
+    }
+    valid = day004.validate_passport(
+        passport, required_keys,
+        validation_rules=validation_rules)
+
+    assert valid is True
+
+
+def test_validate_passport_validation_rules_invalid():
+    passport = {
+        'ecl': 'gry',
+        'pid': '860033327',
+        'eyr': '2020',
+        'hcl': '#fffffd',
+        'byr': '1937',
+        'iyr': '2017',
+        'cid': '147',
+        'hgt': '183cm'
+    }
+    required_keys = ('ecl', 'pid', 'eyr', 'hcl')
+    validation_rules = {
+            'byr': lambda x: 1920 <= int(x) <= 2002,
+            'hcl': lambda x: re.match('#[0-9a-f]{6}', x),
+            'ecl': lambda x: x in ('abc', 'def'),
+    }
+    valid = day004.validate_passport(
+        passport, required_keys,
+        validation_rules=validation_rules)
+
+    assert valid is False
+
+
+def test_validate_height_no_units():
+    value = '136'
+    valid = day004.validate_height(value)
+
+    assert valid is False
+
+
+def test_validate_height_out_of_range():
+    value = '72cm'
+    valid = day004.validate_height(value)
+
+    assert valid is False
+
+
+def test_validate_height_inches():
+    value = '72in'
+    valid = day004.validate_height(value)
+
+    assert valid is True
+
+
+def test_validate_height_centimeters():
+    value = '155cm'
+    valid = day004.validate_height(value)
+
+    assert valid is True
+
+
 def test_count_valid_passports_example():
     passport_data = '''
         ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
@@ -158,3 +253,75 @@ def test_count_valid_passports_example():
     count = day004.count_valid_passports(stream, required_fields)
 
     assert count == 2
+
+
+def test_count_valid_passports_example_part2_invalid():
+    passport_data = '''
+        eyr:1972 cid:100
+        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+        iyr:2019
+        hcl:#602927 eyr:1967 hgt:170cm
+        ecl:grn pid:012533040 byr:1946
+
+        hcl:dab227 iyr:2012
+        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+        hgt:59cm ecl:zzz
+        eyr:2038 hcl:74454a iyr:2023
+        pid:3556412378 byr:2007'''
+    required_fields = [
+        'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    validation_rules = {
+        'byr': lambda x: 1920 <= int(x) <= 2002,
+        'iyr': lambda x: 2010 <= int(x) <= 2020,
+        'eyr': lambda x: 2020 <= int(x) <= 2030,
+        'hgt': day004.validate_height,
+        'hcl': lambda x: re.match(day004.RE_VALID_HAIR_COLOR, x),
+        'ecl': lambda x: x in (
+            'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'),
+        'pid': lambda x: re.match(day004.RE_VALID_PID, x),
+    }
+
+    # Pretend to be a file
+    stream = io.StringIO(passport_data.strip())
+    count = day004.count_valid_passports(
+        stream, required_fields, validation_rules)
+
+    assert count == 0
+
+
+def test_count_valid_passports_example_part2_valid():
+    passport_data = '''
+        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+
+        eyr:2029 ecl:blu cid:129 byr:1989
+        iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+        hcl:#888785
+        hgt:164cm byr:2001 iyr:2015 cid:88
+        pid:545766238 ecl:hzl
+        eyr:2022
+
+        iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+        '''
+    required_fields = [
+        'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    validation_rules = {
+        'byr': lambda x: 1920 <= int(x) <= 2002,
+        'iyr': lambda x: 2010 <= int(x) <= 2020,
+        'eyr': lambda x: 2020 <= int(x) <= 2030,
+        'hgt': day004.validate_height,
+        'hcl': lambda x: re.match(day004.RE_VALID_HAIR_COLOR, x),
+        'ecl': lambda x: x in (
+            'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'),
+        'pid': lambda x: re.match(day004.RE_VALID_PID, x),
+    }
+
+    # Pretend to be a file
+    stream = io.StringIO(passport_data.strip())
+    count = day004.count_valid_passports(
+        stream, required_fields, validation_rules)
+
+    assert count == 4
